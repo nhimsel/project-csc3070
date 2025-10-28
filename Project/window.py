1d1d64ce3698fff8cc09701e0a227175bdf55eac
nhimsel <nathanael.himsel@cuw.edu>
2025-10-28 13:39:21 -0500

    fix so run in qtcreator

--- Project/window.py
+++ Project/window.py
@@ -1,50 +1,14 @@
 import sys
+from PySide6.QtWidgets import QApplication, QSystemTrayIcon
+from test import Window
 
-from PySide6.QtWidgets import QMainWindow, QSystemTrayIcon, QApplication
-from PySide6.QtGui import QIcon
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_Window
-from tray import TrayIcon
-
-
-class Window(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_Window()
-        self.ui.setupUi(self)
-
-        self.setWindowTitle("test")
-        self.ui.lineEdit.textChanged.connect(self.sync_tex)
-
-        # create tray helper (it will be a no-op if system tray is unavailable)
-        self.tray = TrayIcon(self)
-
-    def sync_tex(self):
-        if self.ui.lineEdit.text() != self.ui.lineEdit_2.text():
-            self.ui.lineEdit_2.setText(self.ui.lineEdit.text())
-
-    def closeEvent(self, event):
-        # minimize to tray instead of quitting
-        event.ignore()
-        self.hide()
-        # use tray helper to show an optional message
-        if getattr(self, "tray", None) and self.tray.is_available:
-            self.tray.show_message("test", "Application minimized to tray")
-
-    def on_tray_activated(self, reason):
-        if reason == QSystemTrayIcon.ActivationReason.Trigger:
-            if self.isVisible():
-                self.hide()
-            else:
-                self.showNormal()
-                self.raise_()
-                self.activateWindow()
-
-    def quit_app(self):
-        # hide tray icon before quitting
-        self.tray_icon.hide()
-        QApplication.quit()
+    if QSystemTrayIcon.isSystemTrayAvailable():
+        app.setQuitOnLastWindowClosed(False)
+        win.hide()
+    else:
+        win.show()
+    sys.exit(app.exec())
