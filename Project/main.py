4d233079d83adafc1052362482b456462e822ccf
nhimsel <nathanael.himsel@cuw.edu>
2025-10-28 19:59:56 -0700

    Revert "fix so run in qtcreator"
    This reverts commit 1d1d64ce3698fff8cc09701e0a227175bdf55eac.

---
+++ Project/main.py
@@ -0,0 +1,14 @@
+import sys
+from PySide6.QtWidgets import QApplication, QSystemTrayIcon
+from window import Window
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+
+    if QSystemTrayIcon.isSystemTrayAvailable():
+        app.setQuitOnLastWindowClosed(False)
+        win.hide()
+    else:
+        win.show()
+    sys.exit(app.exec())
