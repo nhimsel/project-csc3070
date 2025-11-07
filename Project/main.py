01ed87d35edc54193e3fb0c7b6511014a98923b3
nhimsel <nathanael.himsel@cuw.edu>
2025-11-07 09:16:56 -0600

    window.py -> chat_window.py

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QSystemTrayIcon
-from window import Window
+from chat_window import Window
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
